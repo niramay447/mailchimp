a7812fd59e9b68edfd297c12232ce5b74c580762
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-05-06 12:57:59 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/", function(req,res){
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+
+    console.log(firstName,lastName,email);
+
+})
+
+
+app.get("/", function(req, res){
+    res.sendfile(__dirname + "/Signup.html")
+});
 app.listen(3000, function(){
     console.log("Server is running on port 3000.");
 });
